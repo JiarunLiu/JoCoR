7f2488e33e9a879c5e0827a392240c18af52a590
JiarunLiu <jiarunliu@foxmail.com>
2020-11-23 22:40:04 +0800

    update train

--- mnist.sh
+++ mnist.sh
@@ -1,7 +1,7 @@
 
-python main.py --dataset mnist --noise_type symmetric --noise_rate 0.2 --n_epoch 320 --co_lambda 0.95 --gpu 0 --result_dir ./results/mnist/sn_0.2 --lr 1e-4
-python main.py --dataset mnist --noise_type symmetric --noise_rate 0.4 --n_epoch 320 --co_lambda 0.95 --gpu 0 --result_dir ./results/mnist/sn_0.4 --lr 1e-4
-python main.py --dataset mnist --noise_type symmetric --noise_rate 0.8 --n_epoch 320 --co_lambda 0.95 --gpu 0 --result_dir ./results/mnist/sn_0.8 --lr 1e-4
+python main.py --dataset mnist --noise_type symmetric --noise_rate 0.2 --n_epoch 320 --co_lambda 0.95 --gpu 0 --result_dir ./results/mnist_SGD/sn_0.2 --lr 1e-4 --optimizer SGD
+python main.py --dataset mnist --noise_type symmetric --noise_rate 0.4 --n_epoch 320 --co_lambda 0.95 --gpu 0 --result_dir ./results/mnist_SGD/sn_0.4 --lr 1e-4 --optimizer SGD
+python main.py --dataset mnist --noise_type symmetric --noise_rate 0.8 --n_epoch 320 --co_lambda 0.95 --gpu 0 --result_dir ./results/mnist_SGD/sn_0.8 --lr 1e-4 --optimizer SGD
 
-python main.py --dataset mnist --noise_type pairflip --noise_rate 0.2 --n_epoch 320 --co_lambda 0.95 --gpu 0 --result_dir ./results/mnist/pair_0.2 --lr 1e-4
-python main.py --dataset mnist --noise_type pairflip --noise_rate 0.45 --n_epoch 320 --co_lambda 0.95 --gpu 0 --result_dir ./results/mnist/pair_0.45 --lr 1e-4
+python main.py --dataset mnist --noise_type pairflip --noise_rate 0.2 --n_epoch 320 --co_lambda 0.95 --gpu 0 --result_dir ./results/mnist_SGD/pair_0.2 --lr 1e-4 --optimizer SGD
+python main.py --dataset mnist --noise_type pairflip --noiseupda_rate 0.45 --n_epoch 320 --co_lambda 0.95 --gpu 0 --result_dir ./results/mnist_SGD/pair_0.45 --lr 1e-4 --optimizer SGD
